6c7e1b27cfb0b0e83935e4d743303610c8456050
Teldoras <105275993+Teldoras@users.noreply.github.com>
2025-04-10 17:07:29 +0700

    Add files via upload

--- booking_data_interactions.js
+++ booking_data_interactions.js
@@ -21,9 +21,9 @@ async function take_tables_data() {
         table.className = 'table'
         table.disabled = 'true'
         table.textContent = element.number
-        table.style.left = (-10 + element.position_x * 20) + '%'
+        table.style.left = (-7 + element.position_x * 16) + '%'
         table.style.top = (-10 + element.position_y * 20) + '%'
-        table.title = 'столик ' + element.number
+        table.title = 'Столик ' + element.number
         hall.appendChild(table)
     });
 
@@ -32,32 +32,41 @@ async function take_tables_data() {
 
 async function take_bookings_data(time) {
     let url = "/take_BD"
-    let response = await fetch(url, {
-        method: "POST",
-        headers: {
-            "Content-Type": "text/json"
-        },
-        body: time
-    })
-
-    let json_bookings_data = await response.json()
-
-    //    console.log('данные о бронировании (json): ' + json_bookings_data)
-    //    console.log('данные о бронировании (json str): ' + JSON.stringify(json_bookings_data))
-    //    console.log('данные о бронировании (json par): ' + JSON.parse(json_bookings_data))
-
-    console.log(json_bookings_data.bookings)
-    console.log(json_bookings_data.not_bookings)
-
+    let response
+    try {
+        let timer
+        response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "text/json"
+            },
+            body: time
+        })
+    
+    } catch (ex) {
+        console.log(ex)
+    }
+    //console.log('response', response)
     let tables = [].slice.call(document.getElementsByClassName('table'))
     tables.forEach(element => {
+        //console.log('cleared', element.id)
+        element.title = `Столик ${element.id.substring(element.id.length - 1)}`
         element.style.backgroundColor = 'azure'
         element.disabled = false
     })
 
+    let json_bookings_data
+    try {
+        json_bookings_data = await response.json()
+    } catch (ex) {
+        console.log(ex)
+    }
+
     let table
     json_bookings_data.bookings.forEach(element => {
+
         table = document.getElementById('table_' + element.id)
+        console.log('bisy', table.id)
         table.style.backgroundColor = 'darkgrey'
         table.disabled = true
         //   table.title += `\nзабронирован\nс ${element.time_from} \nпо  ${element.time_to}`;
@@ -65,40 +74,46 @@ async function take_bookings_data(time) {
     })
     table = null
 
-    let date = new Date()
     let free_tables = [], table_exists
     json_bookings_data.not_bookings.forEach(element => {
-        table_exists = false
-        for (let i = 0; i < free_tables.length; i++) {
-            table = free_tables[i];
-            if (table.id == element.id)
-            {
-                table_exists = true
-                if (element.time_from < table.time_to)
-                    table.time_to = element.time_from
-                else if (element.time_to > table.time_from)
-                    table.time_from = element.time_to
+        try {
+            table_exists = false
+            //console.log('not booking: ', element.id, element.time_from, element.time_to)
+            for (let i = 0; i < free_tables.length; i++) {
+                table = free_tables[i];
+                if (table.id == element.id)
+                {
+                    table_exists = true
+                    if (element.time_from < table.time_to)
+                        table.time_to = element.time_from
+                    else if (element.time_to > table.time_from)
+                        table.time_from = element.time_to
+                }
             }
-        }
-        if (!table_exists) {
-            table = {id: element.id}
-            console.log(date, element.time_from, element.time_to)
-            if (date < element.time_from)
-                table.time_to = element.time_from
-            else if (date > element.time_to)
-                table.time_from = element.time_to
-            console.log('push: ', table)
-            free_tables.push(table)
+            
+            if (!table_exists) {
+                table = {id: element.id}
+                //console.log(time, element.time_from, element.time_to)
+                if (time < element.time_from)
+                    table.time_to = element.time_from
+                else if (time > element.time_to)
+                    table.time_from = element.time_to
+                //console.log('push: ', table)
+                free_tables.push(table)
+            } 
+        } catch (ex) {
+            console.log(ex)
         }
     })
     table = null
 
     free_tables.forEach(element => {
-        console.log(element.id)
+        //console.log(element.id)
         table = document.getElementById('table_' + element.id)
-        console.log(table)
+        //console.log(table)
+        //table.title += `\nсвободен\n`
+        //console.log(element.time_from, element.time_to)
         table.title += `\nсвободен\n`
-        console.log(element.time_from, element.time_to)
         if (element.time_from)
             table.title += `с ${element.time_from} \n`
         if (element.time_to)
--- server/server_booking_data_interactions.cjs
+++ server/server_booking_data_interactions.cjs
@@ -12,6 +12,7 @@ module.exports = function () {
 
         return json;
     }
+
     // получение данных о бронированиях из базы данных
     this.DB_take_BD = async function (time) {
         const client = await connection_module()
@@ -24,6 +25,8 @@ module.exports = function () {
         let booking_data = []
         let tables_res
 
+        //console.log('booking', booking_res.rows)
+
         for (i = 0; i < booking_res.rows.length; i++) {
 
             query_content = `SELECT table_number FROM bookings_tables WHERE booking_id = '${booking_res.rows[i].id}'`;
@@ -34,40 +37,52 @@ module.exports = function () {
             })
         }
 
-        query_content = `
+        //console.log('check')
+
+        let not_booking_res
+        try {
+            query_content = `
             SELECT bookings_tables.table_number AS id, bookings.time_from, bookings.time_to
             FROM  bookings_tables JOIN bookings 
-            ON bookings_tables.booking_id = bookings.id 
-            WHERE bookings_tables.table_number NOT IN (`
-        for (i = 0; i < booking_data.length; i++){
-            console.log('i = ' + i)
-            if (i > 0)
-                query_content += ','
-            console.log('id = ' + booking_data[i].id)
-
-            query_content += booking_data[i].id;
-        }       
-        query_content += `)`
+            ON bookings_tables.booking_id = bookings.id `
+            if (booking_data.length > 0)
+            {
+                query_content += `WHERE bookings_tables.table_number NOT IN (`
+                for (i = 0; i < booking_data.length; i++) {
+                    //console.log('i = ' + i)
+                    if (i > 0)
+                        query_content += ','
+                    //console.log('id = ' + booking_data[i].id)
+    
+                    query_content += booking_data[i].id;
+                }
+                query_content += `)`
+            }
+            not_booking_res = await client.query(query_content)
+        } catch (error) {
+            console.log('error', error)
+        }
 
         //console.log(query_content)
 
-        const not_booking_res = await client.query(query_content)
+        //console.log('not booking', not_booking_res.rows)
 
-        //console.log(not_booking_res.rows)
-
-        let avaiable_tables_data = []
-        for (i = 0; i < not_booking_res.rows.length; i++)
-        {
-            // time
-            avaiable_tables_data.push({id:0, time_from: Math.max(0,0), time_to: Math.min(0,0)})
-        }
+        let not_bookings = []
+        not_booking_res.rows.forEach(element => {
+            not_bookings.push({
+                id: element.id,
+                time_from: element.time_from.substring(0, 5),
+                time_to: element.time_to.substring(0, 5)
+            })
+        });
 
         //console.log(booking_data)
         //console.log(JSON.stringify(booking_data))
         //console.log(JSON.stringify({booking_data:booking_data, not_booking_data: not_booking_res.rows}))
 
-        return JSON.stringify({bookings: booking_data, not_bookings: not_booking_res.rows});
+        return JSON.stringify({ bookings: booking_data, not_bookings: not_bookings });
     }
+
     //отправление данных о новом бронировании в базу данных
     this.DB_send_BD = async function (dataJSON) {
         const client = await connection_module()
@@ -83,15 +98,15 @@ module.exports = function () {
         query_content = `INSERT INTO bookings_tables (booking_id, table_number) VALUES `
         for (let i = 0; i < data.id.length; i++) {
             query_content += `(${booking_id}, ${data.id[i]})`
-            if (i < data.id.length - 1){
+            if (i < data.id.length - 1) {
                 query_content += ','
             }
-            else{
+            else {
                 query_content += ';'
             }
         }
         res = await client.query(query_content)
-        return JSON.stringify({result: true, booking_id: booking_id})
+        return JSON.stringify({ result: true, booking_id: booking_id })
     }
 
     this.connection_module = async function () {
